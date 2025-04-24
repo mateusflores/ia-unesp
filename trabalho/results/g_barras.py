131fe57a903d683180af58e1b29ce529830fbe78
Mateus Flores <mateusgf.42@gmail.com>
2025-04-24 17:09:25 -0300

    Graficos de barras

---
+++ trabalho/results/g_barras.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr, ttest_ind
+import numpy as np
+
+# ===== Definição dos DataFrames individuais =====
+# sa_mht.dat
+data_sa_mht = {
+    'Algorithm': ['SA_Manhattan']*10,
+    'Moves': [10682, 7586, 10271, 36235, 27549, 15364, 11310, 51161, 16534, 6782],
+    'Time': [0.00182, 0.15353, 0.02944, 0.01910, 0.03189, 0.03018, 0.00238, 0.16155, 0.31208, 0.00095],
+    'Result': ['V']*10
+}
+df_sa_mht = pd.DataFrame(data_sa_mht)
+
+# sa_pop.dat
+data_sa_pop = {
+    'Algorithm': ['SA_Pieces_out_of_place']*10,
+    'Moves': [16316, 9766, 707, 6054, 23703, 22276, 12993, 23634, 12208, 3762],
+    'Time': [0.14042, 0.00122, 0.01255, 0.00075, 0.02789, 0.15288, 0.02650, 0.05314, 0.05151, 0.02538],
+    'Result': ['V']*10
+}
+df_sa_pop = pd.DataFrame(data_sa_pop)
+
+#lbs_mht.dat
+data_lbs_mht = {
+    'Algorithm': ['LBS_Manhattan']*10,
+    'Moves': [190851, 190878, 99953, 11988, 99963, 668, 69920, 99955, 69924, 99953],
+    'Time': [0.15245, 0.15225, 0.10189, 0.01901, 0.10227, 0.05129, 0.02891, 0.08539, 0.02708, 0.07068],
+    'Result': ['V']*10
+}
+df_lbs_mht = pd.DataFrame(data_lbs_mht)
+
+#lbs_pop.dat
+data_lbs_pop = {
+    'Algorithm': ['LBS_Pieces_out_of_place']*10,
+    'Moves': [122348, 81162, 172519, 104270, 37740, 59330, 47980, 47974, 46888, 81692],
+    'Time': [0.03403, 0.05827, 0.08678, 0.07642, 0.06614, 0.05441, 0.00737, 0.00726, 0.02257, 0.05801],
+    'Result': ['V']*10
+}
+df_lbs_pop = pd.DataFrame(data_lbs_pop)
+
+#hc_mht.dat
+data_hc_mht = {
+    'Algorithm': ['HC_Manhattan']*10,
+    'Moves': [999482, 128674, 100350, 14342, 999502, 167118, 53110, 750782, 716624, 514626],
+    'Time': [0.17153, 0.02203, 0.01728, 0.00246, 0.17079, 0.02851, 0.00908, 0.12842, 0.11811, 0.07247],
+    'Result': ['D', 'V', 'V', 'V', 'D', 'V', 'V', 'V', 'V', 'V']
+}
+df_hc_mht = pd.DataFrame(data_hc_mht)
+
+#hc_pop.dat
+data_hc_pop = {
+    'Algorithm': ['HC_Pieces_out_of_place']*10,
+    'Moves': [606026, 999900, 93316, 550748, 542050, 807924, 369926, 209836, 13404, 596464],
+    'Time': [0.09200, 0.15211, 0.01411, 0.08349, 0.08241, 0.12297, 0.05494, 0.02708, 0.00190, 0.07534],
+    'Result': ['V', 'D', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V']
+}
+df_hc_pop = pd.DataFrame(data_hc_pop)
+
+#bfs.dat
+data_bfs = {
+    'Algorithm': ['BFS']*10,
+    'Moves': [20, 24, 22, 26, 18, 28, 18, 24, 20, 26],
+    'Time': [11.49168, 132.72770, 35.54142, 387.07862, 3.27248, 570.11209, 1.42791, 130.39138, 8.09516, 292.40911],
+    'Result': ['V']*10
+}
+df_bfs = pd.DataFrame(data_bfs)
+
+# Combinando todos os algoritmos em um único DF
+dfs = [
+    pd.DataFrame(data_sa_mht),
+    pd.DataFrame(data_sa_pop),
+    pd.DataFrame(data_lbs_mht),
+    pd.DataFrame(data_lbs_pop),
+    pd.DataFrame(data_hc_mht),
+    pd.DataFrame(data_hc_pop),
+    pd.DataFrame(data_bfs)
+]
+df_all = pd.concat(dfs, ignore_index=True)
+
+# ===== Estatísticas Descritivas =====
+desc = df_all.groupby('Algorithm').agg(
+    Moves_mean=('Moves', 'mean'),
+    Moves_std=('Moves', 'std'),
+    Time_mean=('Time', 'mean'),
+    Time_std=('Time', 'std'),
+    Success_rate=('Result', lambda x: (x == 'V').mean() * 100)
+)
+
+'''
+# Gráfico de barras para Moves_mean
+desc[['Moves_mean']].plot(kind='bar', figsize=(10, 6))
+plt.title('Comparação de Média de Movimentos por Algoritmo')
+plt.ylabel('Número de movimentos')
+plt.xlabel('Algoritmo')
+plt.xticks(rotation=45)
+plt.legend(['Média de Movimentos'])
+plt.tight_layout()
+plt.show()
+
+# Gráfico de barras para times_mean sem BFS
+desc_sem_bfs = desc.drop(index='BFS', errors='ignore')
+desc_sem_bfs[['Time_mean']].plot(kind='bar', figsize=(10, 6))
+plt.title('Comparação de Média de Tempo por Algoritmo (Sem BFS)')
+plt.ylabel('Tempo (s)')
+plt.xlabel('Algoritmo')
+plt.xticks(rotation=45)
+plt.legend(['Média de Movimentos'])
+plt.tight_layout()
+plt.show()
+
+
+# Gráfico de barras para Time_mean
+desc[['Time_mean']].plot(kind='bar', figsize=(10, 6))
+plt.title('Comparação de Média de Tempo por Algoritmo')
+plt.ylabel('Tempo (s)')
+plt.xlabel('Algoritmo')
+plt.xticks(rotation=45)
+plt.legend(['Média de Tempo'])
+plt.tight_layout()
+plt.show()
+
+
+# Gráfico de barras para Success_rate
+desc[['Success_rate']].plot(kind='bar', figsize=(10, 6))
+plt.title('Comparação de Taxas de sucesso')
+plt.ylabel('Taxa de Sucesso (%)')
+plt.xlabel('Algoritmo')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+'''
+# ============================================
